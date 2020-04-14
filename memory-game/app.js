17ac851e7a4866ae982aecc4fb330f385709e979
SuzannAdams <suzannyee@yahoo.com>
2020-04-14 15:13:44 -0700

    completed the game from tutorial

--- memory-game/app.js
+++ memory-game/app.js
@@ -3,43 +3,117 @@ document.addEventListener('DOMContentLoaded', () => {
   const cardArray = [
     {
       name: 'fries',
-      img: 'images/fries.png'
+      img: 'https://i.imgur.com/Vb8Da9J.png?1',
+    },
+    {
+      name: 'fries',
+      img: 'https://i.imgur.com/Vb8Da9J.png?1',
     },
     {
       name: 'cheeseburger',
-      img: 'images/cheesburger.png'
+      img: 'https://i.imgur.com/6xIG0rl.jpg?3',
+    },
+    {
+      name: 'cheeseburger',
+      img: 'https://i.imgur.com/6xIG0rl.jpg?3',
     },
     {
       name: 'hotdog',
-      img: 'images/hotdog.png'
+      img: 'https://i.imgur.com/QCN8cip.png?1',
+    },
+    {
+      name: 'hotdog',
+      img: 'https://i.imgur.com/QCN8cip.png?1',
     },
     {
       name: 'icecream',
-      img: 'images/icecream.png'
+      img: 'https://i.imgur.com/8cIVaiU.png?1',
+    },
+    {
+      name: 'icecream',
+      img: 'https://i.imgur.com/8cIVaiU.png?1',
     },
     {
       name: 'milkshake',
-      img: 'images/milkshake.png'
+      img: 'https://i.imgur.com/q8YXlHg.png?3',
+    },
+    {
+      name: 'milkshake',
+      img: 'https://i.imgur.com/q8YXlHg.png?3',
     },
     {
       name: 'pizza',
-      img: 'images/pizza.png'
+      img: 'https://i.imgur.com/6xIG0rl.jpg?4',
     },
     {
-      name: 'white',
-      img: 'images/white.png'
-    }
+      name: 'pizza',
+      img: 'https://i.imgur.com/6xIG0rl.jpg?4',
+    },
   ];
+
+  cardArray.sort(() => 0.5 - Math.random());
+
   const grid = document.querySelector('.grid');
+  const resultDisplay = document.querySelector('#result');
+  var cardsChosen = [];
+  var cardsChosenId = [];
+  var cardsWon = [];
 
   //create gameboard
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
-      let card = document.createElement('img');
-      card.setAttribute('src', 'images/blank.png');
+      var card = document.createElement('img');
+      card.setAttribute('src', 'https://i.imgur.com/FXy3wiX.png?2');
       card.setAttribute('data-id', i);
-      // card.addEventListener('click', flipcard)
+      card.addEventListener('click', flipCard);
       grid.appendChild(card);
+      console.log('createBoard');
+    }
+  }
+
+  //check for matches
+  function checkForMatch() {
+    var cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+    if (cardsChosen[0] === cardsChosen[1]) {
+      alert('You found a match');
+      cards[optionOneId].setAttribute(
+        'src',
+        'https://i.imgur.com/ZGdAa0G.png?2'
+      );
+      cards[optionTwoId].setAttribute(
+        'src',
+        'https://i.imgur.com/ZGdAa0G.png?2'
+      );
+      cardsWon.push(cardsChosen);
+    } else {
+      cards[optionOneId].setAttribute(
+        'src',
+        'https://i.imgur.com/FXy3wiX.png?2'
+      );
+      cards[optionTwoId].setAttribute(
+        'src',
+        'https://i.imgur.com/FXy3wiX.png?2'
+      );
+      alert('Sorry, try again');
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cardArray.length / 2) {
+      resultDisplay.textContent = 'Congratulations! You found them all!';
+    }
+  }
+
+  //flip your card
+  function flipCard() {
+    var cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
     }
   }
 
